64409b28f8c1ce3ef1cca54085fc7da2f10cb46a
Yaroslav Pyshnik <yaroslav.pgit@gmail.com>
2022-07-18 21:34:54 +0300

    Final without comments

---
+++ src/Exer.java
@@ -0,0 +1,35 @@
+public class Exer {
+    //        Main main1 = new Main();
+//
+//        String a = "Hello world";// Интернирование
+//        String b = new String("Hello world");
+//
+//        //ctrl + P посмотреть варианты в скобках
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(a == b);
+//        System.out.println(a.equals(b));
+//        System.out.println("Приведение к верхнему регистру " + a.toUpperCase());
+//        System.out.println("Приведение к нижнему регистру " + a.toLowerCase());
+//        System.out.println("Вырезание подстроки из строки " + a.substring(2));
+//        System.out.println("Получение массива байтов строки " + Arrays.toString(a.getBytes()));
+//        System.out.println("Пустая строка или нет (не учитывает крайние пробелы) " + a.isBlank());
+//        String c = "";
+//        String d = " a ";
+//        System.out.println("Получение длинны строки " + c.length());
+//        System.out.println(d.trim() + "trim обрезает крайние пробелы справа и слева " + a.length());//нужный метод!
+//        System.out.println("Пустая строка или нет (!!!учитывает крайние пробелы!!!) " + a.isEmpty());
+//        System.out.println("Замена элементов строки " + a.replace("l", "x"));
+//        System.out.println("Проверка начинается ли строка с переданного литерала "+ a.startsWith("Hello"));
+//        System.out.println("Проверка заканчивается ли строка переданным литералом " + a.endsWith("world"));
+//        System.out.println("Объедение строк "+ a.concat("123"));
+//        System.out.println("Объединение строк - " + a + "123");
+//        System.out.println("Целочисленное сравнение строк " + a.compareTo("Hello world45sdf"));
+//        System.out.println("Получение символа по индексу " + a.charAt(5));
+//        System.out.println("Есть ли в строке переданная строка " + a.contains("Hello"));
+//        System.out.println("Индекс вхождения первого символа " + a.indexOf("world"));
+//        System.out.println("Разбиение строки на массив строк по разделителю " + Arrays.toString(a.split(" ")));
+//        StringBuilder st = new StringBuilder("Hello World");
+//        System.out.println(st.append("1234").append("dafaf").append("sdfsdf"));
+
+}
